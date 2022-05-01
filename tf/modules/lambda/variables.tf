93472f9514ead4a6f39559dac45092efaf1d02a8
Dan Munro <dan@danmunro.com>
2022-04-30 19:34:21 -0700

    add some details

---
+++ tf/modules/lambda/variables.tf
@@ -0,0 +1,11 @@
+variable "project" {
+  type        = string
+  description = "Project name"
+  default     = "backstage"
+}
+
+variable "region" {
+  type        = string
+  description = "AWS region"
+  default     = "us-west-2"
+}
